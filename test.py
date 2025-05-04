c7f0ba26d178951493532913d24f435c27cab77a
AmrElsherbiny <amoryelsherbiny@gmail.com>
2025-05-04 13:04:07 +0300

    typecasting

--- test.py
+++ test.py
@@ -1,14 +1,23 @@
 # print("Hello, World!")
 
 # variables 
-fullName = "Amr"
+# fullName = "Amr"
 
-lastName = "elsherbiny"
+# lastName = "elsherbiny"
 
-age = 19
+# age = 19
 
-gpa = "0.0001"
+# gpa = "0.0001"
 
-IsStudent = True
+# IsStudent = True
 
-print(f"Hello {fullName} { lastName} your age is {age} and your gpa is {gpa} and is you are student ? {IsStudent}")
+# print(f"Hello {fullName} { lastName} your age is {age} and your gpa is {gpa} and is you are student ? {IsStudent}")
+
+
+
+# typecasting 
+price = 30.5  # float variable
+
+price = int(price) # convert to int
+
+print(price) # 30
