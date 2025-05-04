0aa9a93517b84780aab1f701b575f79606669740
AmrElsherbiny <amoryelsherbiny@gmail.com>
2025-05-04 13:00:26 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("Hello, World!")
+# print("Hello, World!")
+
+# variables 
+fullName = "Amr"
+
+lastName = "elsherbiny"
+
+age = 19
+
+gpa = "0.0001"
+
+IsStudent = True
+
+print(f"Hello {fullName} { lastName} your age is {age} and your gpa is {gpa} and is you are student ? {IsStudent}")
